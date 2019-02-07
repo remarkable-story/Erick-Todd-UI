9ab0b604ccd6fb1e85754082c9411bdf9ee86dcd
ddot727 <ddot727@gmail.com>
2019-02-07 18:30:07 -0500

    still tweaking

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ accordion.showOne();
 //------Tableau------//
 
 var divElement = document.getElementById('viz1549401526339');
-var vizElement = divElement.getElementsByTagName('object')[0]; vizElement.style.width = '100%'; 
+var vizElement = divElement.getElementsByTagName('object')[0]; 
+
+vizElement.style.width = '100%'; 
 vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+
 var scriptElement = document.createElement('script'); 
 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
