178fd0eb8b6827be4738c9d5919aaea71a46c873
ddot727 <ddot727@gmail.com>
2019-02-07 11:02:02 -0500

    Polished of project and linked the Sign Up Button

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-//----------Accordion-----////
+//-----Accordion-----//
+
 class Accordion {
-  constructor(heading) {
-     this.heading = heading;
-  }
-  
-  showOne() {
-     const accordionHeading = document.querySelectorAll(this.heading);
-     
-     accordionHeading.forEach((item, key) => {
-        item.addEventListener('click', () => { 
-           accordionHeading.forEach(element => {
-              element.classList.contains('active') ? 
-                 element.classList.remove('active') : null;
-           });
-           
-           item.classList.add('active');
-        });
-     });
-  }
+   constructor(heading) {
+      this.heading = heading;
+   }
+
+   showOne() {
+      const accordionHeading = document.querySelectorAll(this.heading);
+
+      accordionHeading.forEach((item, key) => {
+         item.addEventListener('click', () => {
+            accordionHeading.forEach(element => {
+               element.classList.contains('active') ?
+                  element.classList.remove('active') : null;
+            });
+
+            item.classList.add('active');
+         });
+      });
+   }
 }
 const accordion = new Accordion('.title');
 accordion.showOne();
+
+
+
+//------Tableau------//
+
+var divElement = document.getElementById('viz1549401526339');
+var vizElement = divElement.getElementsByTagName('object')[0]; vizElement.style.width = '100%'; 
+vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+var scriptElement = document.createElement('script'); 
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
