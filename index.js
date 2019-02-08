d01c0155429529310b3aab94cb00cb298bee89b0
ddot727 <ddot727@gmail.com>
2019-02-08 12:04:38 -0500

    still tweaking

--- index.js
+++ index.js
@@ -22,16 +22,3 @@ class Accordion {
 }
 const accordion = new Accordion('.title');
 accordion.showOne();
-
-
-
-//------Tableau------//
-
-var divElement = document.getElementById('viz1549401526339');
-var vizElement = divElement.getElementsByTagName('object')[0]; 
-
-vizElement.style.width = '100%'; 
-vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-
-var scriptElement = document.createElement('script'); 
-scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; vizElement.parentNode.insertBefore(scriptElement, vizElement);
